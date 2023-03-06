e4eba193de398afe77f7e15c302b28771e5eb179
Furi Joshua <furijosh@gmail.com>
2023-03-06 13:38:17 -0400

    Added onReady event in ContextMenuCommands

--- src/main/java/com/veryrarejosh/veryrarebot/commands/ContextMenuCommands.java
+++ src/main/java/com/veryrarejosh/veryrarebot/commands/ContextMenuCommands.java
@@ -1,7 +1,14 @@
 package com.veryrarejosh.veryrarebot.commands;
 
 import net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.Command;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContextMenuCommands extends ListenerAdapter {
     @Override
@@ -10,4 +17,13 @@ public class ContextMenuCommands extends ListenerAdapter {
             event.reply(event.getTarget().getEffectiveAvatarUrl()).queue();
         }
     }
+
+    @Override
+    public void onReady(ReadyEvent event) {
+        List<CommandData> commandData = new ArrayList<>();
+        commandData.add(Commands.context(Command.Type.USER, "Grab user avatar"));
+
+        event.getJDA().updateCommands().addCommands(commandData).queue();
+    }
 }
+
